c6e655e2ee4e441115fee1183e58a0da2a630c2d
Jash Palavwala <jashpalavwala39@gmail.com>
2021-07-05 23:04:51 +0530

    react scroll added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import logo from "../logo.png";
+import { Link } from 'react-scroll';
 function Header() {
   return (
     <>
@@ -8,7 +9,16 @@ function Header() {
           <img src={logo} alt='JP' />
         </div>
         <div className='container-contact'>
+          <Link
+              className='link'
+              activeClass='active'
+              to='contact'
+              smooth={true}
+              duration={1000}
+              spy={true}
+            >
           <span>Contact</span>
+            </Link>
         </div>
       </section>
     </>
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Icon } from "@iconify/react";
 import arrowDown from "@iconify/icons-bi/arrow-down";
+import { Link } from 'react-scroll';
 function Main() {
   return (
     <>
@@ -12,7 +13,16 @@ function Main() {
           I am a Desginer and a Front-end Developer
         </p>
         <div className='main-button-container'>
+          <Link
+              className='link'
+              activeClass='active'
+              to='portfolio'
+              smooth={true}
+              duration={1000}
+              spy={true}
+            >
           <span>View my work</span>
+            </Link>
           <Icon
             icon={arrowDown}
             style={{ paddingLeft: "20px", fontSize: "40px" }}
